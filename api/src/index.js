4cd749dcff29d1708dfaf07e189898c7a60bb6e0
NikolVasileva <nikol.vasileva9@gmail.com>
2025-10-18 18:28:45 +0300

    Add and config database

--- api/src/index.js
+++ api/src/index.js
@@ -1,9 +1,23 @@
 import express from "express";
 import cors from "cors"
 import routes from "./routes.js";
+import mongoose from "mongoose"
 
 
 const app = express();
+
+// Setup mongoose
+try {
+    await mongoose.connect("mongodb://localhost:27017", {
+        dbName: "furniture-workshop"
+    });
+
+    console.log("Successfully connected to DB!")
+
+} catch(err) {
+    console.log("Cannot connect to DB!");
+    console.log(err.message)
+}
 
 // Add CORS
 app.use(cors())
