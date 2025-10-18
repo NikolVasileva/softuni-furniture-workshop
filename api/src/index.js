9d29efbbfe2d3a3b0f9d2790012a91298b6a8778
NikolVasileva <nikol.vasileva9@gmail.com>
2025-10-18 17:55:39 +0300

    Add furniture controller

--- api/src/index.js
+++ api/src/index.js
@@ -1,13 +1,14 @@
 import express from "express";
 import cors from "cors"
+import routes from "./routes.js";
 
 
 const app = express();
 
+// Add CORS
 app.use(cors())
 
-app.get("/", (req, res) => {
-    res.send("it works")
-})
+// Add routes
+app.use(routes)
 
 app.listen(3030, () => "Server is listening to http://localhost:3030...")
